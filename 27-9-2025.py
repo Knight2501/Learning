de4097b227cff488730b112a0bc56eb10065f39e
Knight2501 <tuanhla.ptit@gmail.com>
2025-09-27 02:05:42 +0700

    Add files via upload
    Day 2

---
+++ 27-9-2025.py
@@ -0,0 +1,13 @@
+#Bai 1 dung vong lap while
+count = 1
+while count <= 10:
+        print(count)
+        count = count + 1
+
+#Bai 2 dung vong lap tao thanh rows and numbers
+rows = 5
+for i in range(1, rows + 1,1):
+        for j in range(i, i + 1):
+                print(j, end ='')
+                print(' ')
+
